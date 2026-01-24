148aaf5786b9711ac6129025b9cba23e18685505
Aditi Goyal <143211287+aditiigoyall@users.noreply.github.com>
2026-01-24 14:56:05 +0530

    Update dataset.sql

--- sql/dataset.sql
+++ sql/dataset.sql
@@ -1,1 +1,46 @@
+-- =====================================
+-- DATASET SETUP
+-- =====================================
 
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    country VARCHAR(50)
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    item VARCHAR(50),
+    amount INT,
+    customer_id INT
+);
+
+CREATE TABLE Shippings (
+    shipping_id INT PRIMARY KEY,
+    status VARCHAR(20),
+    customer INT
+);
+
+
+INSERT INTO Customers VALUES
+(1, 'John', 'Doe', 31, 'USA'),
+(2, 'Robert', 'Luna', 22, 'USA'),
+(3, 'David', 'Robinson', 22, 'UK'),
+(4, 'John', 'Reinhardt', 25, 'UK'),
+(5, 'Betty', 'Doe', 28, 'UAE');
+
+INSERT INTO Orders VALUES
+(1, 'Keyboard', 400, 4),
+(2, 'Mouse', 300, 4),
+(3, 'Monitor', 12000, 3),
+(4, 'Keyboard', 400, 1),
+(5, 'Mousepad', 250, 2);
+
+INSERT INTO Shippings VALUES
+(1, 'Pending', 2),
+(2, 'Pending', 4),
+(3, 'Delivered', 3),
+(4, 'Pending', 5),
+(5, 'Delivered', 1);
