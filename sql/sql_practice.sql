2117e782858e63bf0f3d8dd421437b1eca7f8451
Aditi Goyal <143211287+aditiigoyall@users.noreply.github.com>
2026-01-24 13:17:04 +0530

    Update sql_practice.sql

--- sql/sql_practice.sql
+++ sql/sql_practice.sql
@@ -1,1 +1,29 @@
+-- =====================================
+-- SELECT / WHERE / ORDER BY
+-- =====================================
+
+-- Display only the first name and country of all customers.
+SELECT first_name,
+country
+FROM Customers;
+
+-- Find customers whose age is less than 30.
+SELECT *
+FROM Customers
+WHERE age < 30;
+
+-- Display customers whose country is either USA or UK.
+SELECT *
+FROM Customers 
+WHERE country IN ('USA', 'UK');
+
+-- Show all customers ordered by first name alphabetically.
+SELECT *
+FROM Customers 
+ORDER BY first_name ASC;
+
+-- Display customers whose age is NOT NULL.
+SELECT * 
+FROM Customers
+WHERE age IS NOT NULL;
 
