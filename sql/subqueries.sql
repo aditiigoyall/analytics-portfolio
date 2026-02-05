2f5780faef65a43e68ce07e402dd2feb8375267f
Aditi Goyal <143211287+aditiigoyall@users.noreply.github.com>
2026-02-05 23:12:07 +0530

    Update subqueries.sql

--- sql/subqueries.sql
+++ sql/subqueries.sql
@@ -16,31 +16,6 @@ amount
 FROM Orders
 WHERE amount < (SELECT MAX(amount) FROM Orders);
 
--- Find customers who have placed an order for an item priced above 300.
-SELECT c.customer_id,
-c.first_name,
-o.amount
-FROM Customers c
-INNER JOIN Orders o
-ON c.customer_id = o.customer_id
-WHERE o.amount > 300;
-
--- Find customers whose customer_id appears in the Orders table.
-SELECT DISTINCT c.customer_id,
-c.first_name
-FROM Customers c
-INNER JOIN Orders o
-ON c.customer_id = o.customer_id
-ORDER BY c.customer_id;
-
--- Find customers whose customer_id does NOT appear in the Orders table.
-SELECT c.customer_id,
-c.first_name
-FROM Customers c
-LEFT JOIN Orders o 
-ON c.customer_id = o.customer_id
-WHERE o.customer_id IS NULL;
-
 -- Find customers whose order amount is higher than the average order amount.
 SELECT c.customer_id,
 c.first_name,
